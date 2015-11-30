cfeeda05b88fe8fc82a184f5f5818b464f7f9787
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-30 13:09:46 -0500

    helper test

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 class Helpers
 
   def self.current_user(session_info)
-    @user = User.find_by_id(session_info[:user_id])
+    @user = User.find_by_id(session_info["user_id"])
   end
 
   def self.is_logged_in?(session_info)
-    !!session_info[:user_id]
+    !!session_info["user_id"]
   end
 
 end
