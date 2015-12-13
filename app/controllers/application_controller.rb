bef2bfaab741cdbe26026cdd92e9183b8f0d5a5e
jakebrady5 <jakebrady5@gmail.com>
2015-12-13 16:18:34 -0500

    fixed view filepath

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
+    set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
