8908ea7fbd56753741482875391d309e2083278a
frankNowinski <nowinski.frank@gmail.com>
2015-12-24 01:46:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
-
     if !!@user 
       session[:user_id] = @user.id 
       redirect to '/account'
@@ -31,7 +30,7 @@ class ApplicationController < Sinatra::Base
 
   get '/logout' do
     session.clear
-    redirect '/'
+    redirect to '/'
   end
 
 end
