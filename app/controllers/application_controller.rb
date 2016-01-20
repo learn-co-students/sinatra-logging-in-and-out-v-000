cb154026fc3bf9b39f8c38c233ff63b5e02b4093
John Anthony Rivera <askinganthony@gmail.com>
2016-01-20 10:32:45 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,17 +10,43 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  post '/login' do
 
+  post '/login' do
+    
+    #binding.pry
+    if User.exists?(username: params[:username], password: params[:password])
+      @current_user=User.find_by(username: params[:username], password: params[:password])
+      session[:user_id]=@current_user.id
+      redirect to '/account'
+    else
+     erb :error
+    end
+  end
+
+  post '/account' do
+    #@user=User.find(params[:user_id])
+    erb :account
   end
 
   get '/account' do
-
+    if Helpers.user_valid?(session)
+      @user=Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
+
+  post '/error' do
+    erb :error
+  end
+
+
 
 
 end
