e61ade141acbe7c2407e68e080fd4756210c77b4
Karuna <imkaruna@gmail.com>
2016-01-28 00:20:46 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
-class Helpers
+require_relative '../models/user.rb'
+require 'pry'
+
+class Helpers 
+  def self.current_user(session)
+    #binding.pry
+    @user = User.find(session["user_id"])
+    return @user
+  end
+  
+  def self.is_logged_in?(session)
+    #binding.pry
+    @user = User.find(session["user_id"])
+    @user != nil ? true : false
+  end
 end
