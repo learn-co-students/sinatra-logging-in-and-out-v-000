c07c336e0493284a496abf14a74f899e524d0bfb
lizdenhup <lizdenhup@gmail.com>
2016-02-01 16:48:14 -0600

    Sinatra logging in and out lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -10,17 +11,29 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  post '/login' do
+  post '/login' do 
+  @user = User.find_by(username: params[:username])
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect '/account'
+  else 
+    erb :error 
+  end 
+end 
 
-  end
+  get '/account' do 
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
+  end 
 
-  get '/account' do
-
-  end
-
-  get '/logout' do
-
-  end
+  get '/logout' do 
+    session.clear 
+    redirect to '/'
+  end 
 
 
 end
