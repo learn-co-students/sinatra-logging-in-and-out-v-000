cf325c5287fc1d03eea747cfc60c03c40395153a
benser1 <briganenser28@gmail.com>
2016-02-02 16:42:25 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
-class Helpers
+class Helpers 
+  def self.current_user(session)
+    @user = User.find_by_id(session["user_id"])
+  end
+
+  def self.is_logged_in?(session)
+    !!current_user(session)
+  end
 end
