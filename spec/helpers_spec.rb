3131b279225c61063092ce9fe34d20a2b97485d4
Rawradin <edgar.rawr@gmail.com>
2016-02-03 22:35:45 -0800

    Done.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,7 +5,7 @@ describe "Helpers" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
       session = {
-        "user_id" => 1
+        :user_id => 1
       }
       expect(Helpers.current_user(session)).to be_an_instance_of(User)
     end
@@ -17,7 +17,7 @@ describe "Helpers" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
       session = {
-        "user_id" => 1
+        :user_id => 1
       }
       expect(Helpers.is_logged_in?(session)).to eq(true)
     end
