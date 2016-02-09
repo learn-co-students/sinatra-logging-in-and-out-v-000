1550fd17a0cfbb267982e137fca7ec1fdea984c9
KhalilC <klcrumpler@gmail.com>
2016-02-09 02:50:34 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
+require 'pry'
 class Helpers
+
+  def self.current_user(session)
+   @current_user = User.find(session['user_id'])
+ 
+  end
+
+  def self.is_logged_in?(session)
+    !!@current_user
+  end
 end
