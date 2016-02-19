e4ff0b09a2fb4e969a2cb6f51d7efa2bf9db5f0e
Tsundu <osmentdan@gmail.com>
2016-02-18 23:27:11 -0500

    logged in and out

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,20 @@
 class Helpers
+  
+  def self.current_user(session)
+   
+   @current =  User.find(session["user_id"])
+   @current  
+  end
+
+
+  def self.is_logged_in?(session)
+    if session["user_id"] != nil 
+      true 
+    else 
+      false 
+    end 
+  end
+
+
+
 end
