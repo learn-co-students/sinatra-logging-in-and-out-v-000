57c51203f1e26d75ce0fff1b00b6aa2f102be082
the-widget <brennenawana108ny@gmail.com>
2016-03-16 23:07:08 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find_by_id(session["user_id"])
+  end
+
+  def self.is_logged_in?(session)
+    !!session["user_id"]
+    # if self.current_user(session).id == session["user_id"]
+    #   true
+    # else
+    #   false
+    # end
+  end
+
+
 end
