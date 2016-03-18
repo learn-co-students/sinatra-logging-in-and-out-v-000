c8d4a1441cad195fae5a33aac4e3b63e8e764ca7
franco84 <franco84@gmail.com>
2016-03-18 01:42:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,29 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do
+ get '/' do
     erb :index
   end
 
   post '/login' do
-
+    if @user = User.find_by(username: params["username"], password: params["password"])
+      session[:user_id] = @user.id
+      redirect to ("/account")
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if @user = User.find_by_id(session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
