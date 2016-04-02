f7bc89095840bc01e9ce7031a815a8317e660080
glee38 <glee3388@gmail.com>
2016-04-02 11:38:15 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+require 'pry'
+
 class Helpers
+  def self.current_user(session)
+    @current_user = User.find_by(id: session[:user_id])  
+  end
+
+  # !!:
+  # returns true if the object on the right is not nil and not false, false if it is nil or false
+  
+  def self.is_logged_in?(session)
+    !!@current_user
+  end
 end
