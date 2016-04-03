bb36f287efe32277daae5ddff087badd388a3345
morgvanny <morgvanny@gmail.com>
2016-04-02 21:03:39 -0700

    Done.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,7 +5,13 @@ describe "Helpers" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
       session = {
-        :user_id => 1
+        "session_id"=>"40d7f05911859eec423c18b8ca92da192e772a8afe4d1afcec611ae16e483843",
+        "csrf"=>"a8f2c70d597115645c1f97e8ddc6079c", 
+        "tracking"=> { 
+          "HTTP_USER_AGENT"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709", 
+          "HTTP_ACCEPT_LANGUAGE"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709"
+          },
+        "user_id" => 1
       }
       expect(Helpers.current_user(session)).to be_an_instance_of(User)
     end
@@ -13,11 +19,17 @@ describe "Helpers" do
   end
 
   describe 'Helpers#is_logged_in?' do 
-    it "returns true or false" do
+    it "returns the current user" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
       session = {
-        :user_id => 1
+        "session_id"=>"40d7f05911859eec423c18b8ca92da192e772a8afe4d1afcec611ae16e483843",
+        "csrf"=>"a8f2c70d597115645c1f97e8ddc6079c", 
+        "tracking"=> { 
+          "HTTP_USER_AGENT"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709", 
+          "HTTP_ACCEPT_LANGUAGE"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709"
+          },
+        "user_id" => 1
       }
       expect(Helpers.is_logged_in?(session)).to eq(true)
     end
