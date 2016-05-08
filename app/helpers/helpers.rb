fb65c1337c7021a1cdf5c528d372deedb07b7eb4
tttra <tothutra@gmail.com>
2016-05-08 19:59:20 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session_hash)
+    User.find_by_id(session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    if session_hash[:user_id].nil?
+      false
+    else
+      true
+    end
+  end
 end
