b5ab9d50e18288c4725f0bcbfc3bd4ab2edc383d
reifnotreef <bridgerrhammond@gmail.com>
2016-05-19 21:38:28 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,21 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user != nil
+    @user = User.find_by(:username => params[:username])  #set @user to user who logs in
+    if @user != nil #if user exists set session user_id and go to account ROUTE
       session[:user_id] = @user.id
-      redirect to '/account'
+      redirect to '/account' 
+    else #else error page
+      erb :error
     end
-    erb :'views/error'
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id]) #define current user
+    if @current_user
+      erb :account #if real go to account page
+    else
+      erb :error #else error page
+    end
   end
 
   get '/logout' do
     session.clear
   end
-
-
 end
