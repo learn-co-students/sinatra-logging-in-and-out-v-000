e99de8b0d740193341d028efce034f8adf54a419
Saul Ocampo <digitalh2o@gmail.com>
2016-06-14 18:21:27 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+require 'pry'
 class Helpers
+
+	def self.current_user(session)
+		#binding.pry
+		@user = User.find_by_id(session[:user_id])
+	end
+
+	def self.is_logged_in?(session)
+		#binding.pry
+		!!session[:user_id]
+	end
+
 end
