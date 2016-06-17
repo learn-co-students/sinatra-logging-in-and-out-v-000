d137029e5838d78a20ef209fff1d3a76e6f6477a
Puna Earl <earlleah@gmail.com>
2016-06-17 14:19:17 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    true if session_hash[:user_id]
+  end
 end
