f8643018db692fcfd04134573f1d85eab281df6b
wook kim <wooke2e@gmail.com>
2016-07-07 16:14:14 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,13 @@
+require 'sinatra/base'
+
 class Helpers
 
-  def current_user
+  def self.current_user(session_info)
+    @user = User.find_by_id(session_info[:user_id])
   end
 
-  def is_logged_in?
+  def self.is_logged_in?(session_info)
+    !!session_info[:user_id]
   end
-  
+
 end
