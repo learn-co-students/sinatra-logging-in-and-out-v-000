978f2e454053a23b6145a506a9613bc827a6a6a1
steveafrost <ffrostt@gmail.com>
2016-07-16 18:58:07 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(session_hash)
+    User.find_by_id(session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    #double bang negates the negation. Provides true boolean of anything
+    !!session_hash[:user_id]
+  end
 end
