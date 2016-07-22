f182d04e2664485dc030e6010e738b18b03bf7a0
tpstar <hanlee12@gmail.com>
2016-07-22 00:20:40 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,34 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
 
+
   get '/' do
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
-
