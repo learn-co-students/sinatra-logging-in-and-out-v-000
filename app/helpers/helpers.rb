ab630955c87183ea2241525600bb4ab3fe31d33e
Adam Taitano <adam.taitano@gmail.com>
2016-07-28 03:59:35 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
   def self.current_user(argument)
-    @user = User.find_by(id: argument["user_id"])
+    @user = User.find_by_id(id: argument["user_id"])
   end
 
   def self.is_logged_in?(session)
