216b83201bf6b4ed9bb4a6ef53425eea27d3c975
toanhtran <toanh.t.tran@gmail.com>
2016-07-30 19:30:15 +0900

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session_info)
+    @user = User.find_by_id(session_info[:user_id])
+    @user
+  end
+
+  def self.is_logged_in?(session)
+    !!current_user(session)
+  end
 end
