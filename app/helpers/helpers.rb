68345d2183e0a37335792d53089638d3a12fd2cc
dannyenfp <daniel.valmas@gmail.com>
2016-08-09 16:50:43 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
-class Helpers
+class Helpers < User 
+
+def self.current_user(session_hash)
+  user = User.find_by(:id => session_hash[:user_id]) 
+  user
+end 
+
+def self.is_logged_in?(session_hash)
+  if session_hash[:user_id]
+    true
+  else
+    false
+  end
+end  
+
 end
