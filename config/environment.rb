94f469c2fc0d0156ee187f244241d8376a4958a3
David Ward <808dward@gmail.com>
2016-08-10 03:47:57 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  configure :development do
+  set :database, "sqlite3:db/users.db"
+  end
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
