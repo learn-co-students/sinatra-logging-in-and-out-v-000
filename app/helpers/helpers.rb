e4d58d253324ed67d93f2f9305f1de8ef8229b84
Shmuwol <shmullywolfson@gmail.com>
2016-08-18 11:08:02 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers
   def self.current_user(session)
-    User.find(session[:user_id])
+    # User.find(session[:user_id])
+    @current_user ||= User.find(session[:id])
   end
 
   def self.is_logged_in?(session)
