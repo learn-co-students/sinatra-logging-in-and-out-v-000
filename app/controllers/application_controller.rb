8e923df41ef4d7fa0ec1d191fcc25296703d50fd
Shmuwol <shmullywolfson@gmail.com>
2016-08-22 19:58:00 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
+
+    if @current_user = User.find_by_id(session[:user_id])
       erb :account
     else
       erb :error
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,6 @@
 class Helpers
   def self.current_user(session)
-    # User.find(session[:user_id])
-    @current_user ||= User.find(session[:id])
+    @current_user = User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
