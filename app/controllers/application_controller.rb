44900f4f80f745fc14517ca2385f8759d52de3cd
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-28 05:38:06 +0000

    added password argument to post '/login'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
