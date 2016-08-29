6c10ee7ef53b38e38f854e874bfa371c42d27003
ThisIsTyDell <milleb94@gmail.com>
2016-08-29 13:54:27 -0500

    add password validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user != nil
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user != nil 
       session[:user_id] = @user.id
       redirect to '/account'
     else
