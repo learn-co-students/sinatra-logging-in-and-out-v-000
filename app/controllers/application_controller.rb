140c6beeb06cee69d43e735fcd22862043343857
ThisIsTyDell <milleb94@gmail.com>
2016-08-29 13:07:49 -0500

    search user by id and assign to instance variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,17 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    session[:id] = @user.id
+    if @user != nil
+      session[:id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    @logged_in_user = User.find_by_is(session[:id])
+    erb :account
   end
 
   get '/logout' do
