764948cb47029635e89560655bdb3dd84a83c5a0
Ben Norris <bennorrismisc@gmail.com>
2016-09-07 14:09:48 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(hash)
+    @user = User.find_by(id: hash[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!self.current_user(session)
+  end
+
 end
