b56751766ee46a008ebc5ef528f06083d17f1b13
Perla Yates <pianistps@yahoo.com>
2016-10-19 20:30:54 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def current_user(session_hash)
+    @user = User.find_by_id(session_hash["user_id"])
+  end
+
+  def is_logged_in?(session_hash)
+    !!session_hash["user_id"]
+  end
 end
