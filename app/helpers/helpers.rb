4cf3fd5eac2611243dd6f98f8ade0b2a386e496f
William Saxe <geosaxe@gmail.com>
2016-11-05 21:58:41 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
+require 'sinatra/base'
+
 class Helpers
+
+  def self.current_user(session_info)
+    @user = User.find_by_id(session_info[:user_id])
+  end
+
+  def self.is_logged_in?(session_info)
+    if session_info[:user_id] == nil
+      false
+    else
+      true
+    end
+  end
+
 end
