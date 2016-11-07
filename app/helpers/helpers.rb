0bfa741d85296fc90d58e89e50c31c047ffb189e
jasoncasares <jasonc@csdrafting.com>
2016-11-07 08:24:15 -0800

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session_info)
+    @user = User.find_by_id(session_info[:user_id])
+  end
+
+  def self.is_logged_in?(session_info)
+    !!session_info[:user_id]
+  end
 end
