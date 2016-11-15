690b063c7cfa902bc80b2cc997308638052e8f10
Bruno Boehm <bruno.boehm@gmail.com>
2016-11-15 15:39:23 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    session[:user_id] ? true : false
+  end
 end
