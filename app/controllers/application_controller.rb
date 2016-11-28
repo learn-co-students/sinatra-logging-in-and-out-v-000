86c00d6921a38eb0aa1c5c3f53aa15aa98b1f547
William Story <wstory@villanova.edu>
2016-11-28 22:14:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"], password: params["password"])
+    session[:user_id] = @user.id
+    if Helpers.is_logged_in?(session)
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
-
