ff4520bc64ac6c9e68c958f0f6be5b84d354c290
amcooper <amcooper@gmail.com>
2016-12-05 23:14:38 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require_relative '../../config/environment'
+
 class Helpers
+
+  def self.current_user(session)
+    @user = session[:user_id] == nil ? nil : User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    self.current_user(session) != nil
+  end
+
 end
