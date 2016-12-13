e25ae0d48b5e854443b176b75041e3adf948847f
Jon Fougner <jon.fougner@gmail.com>
2016-12-13 05:45:18 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+require 'pry'
+
 class Helpers
+
+  def self.current_user(session_hash)
+    @user = User.find_by(session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    # binding.pry
+    session_hash[:user_id] == @user.id
+  end
+
 end
