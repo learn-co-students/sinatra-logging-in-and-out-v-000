1f27ec024169e55629debbe78abeb69de24a7079
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-12-14 23:26:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if !@user.nil?
+      session[:user_id] = @user.id
+      redirect to '/account'
+    elsif @user.nil?
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
-
