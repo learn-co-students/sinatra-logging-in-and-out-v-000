b1388f0f1e03a1774097b3b4fa0a49a5b4fc7481
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-18 00:43:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user.class != NilClass
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(session_params)
+    User.find(session_params[:user_id])
+  end
+
+  def self.is_logged_in?(session_params)
+    session_params[:user_id] != nil
+  end
+
 end
