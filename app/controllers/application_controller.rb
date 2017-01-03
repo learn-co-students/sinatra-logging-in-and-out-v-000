9f7f9acae44f5bb54d96d1ccc635e08611e735b2
Christina L. Cole <christinalcole@gmail.com>
2017-01-03 18:17:22 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
-
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,9 @@
 class Helpers
-  def self.current_user(session_hash) #This method should use the user_id from the session hash to find the user in the database and return that user.
+  def self.current_user(session_hash) # uses the user_id from the session hash to find the user in the database and return that user.
     @user = User.find_by_id(session_hash[:user_id])
   end
 
-  def self.is_logged_in?(session_hash)  #This method should return true if the user_id is in the session hash and false if not.
-    # !!(self.current_user(session_hash))
+  def self.is_logged_in?(session_hash)  # returns true if the user_id is in the session hash and false if not.
     !!session_hash[:user_id]
   end
 end
