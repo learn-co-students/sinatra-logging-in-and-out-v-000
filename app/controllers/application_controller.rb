e5f709dac42f2ce87e60cb5257bcd1058061bf97
Pia Kim <pkim2320@gmail.com>
2017-01-06 04:36:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,37 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+  #  puts params
+    # @user = User.find_by(username: params["username"])
+    # if User.find_by(username: params["username"], password: params["password"])
+    #   session[:user_id] = @user.id
+    #   redirect to '/account'
 
+    @user = User.find_by(:username => params[:username])
+
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
+    # from solution:
+    # @current_user = User.find_by_id(session[:user_id])
+    # if @current_user
+    #  erb :account
+    if Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find_by(id: session[:user_id])
+    @user
+  end
+
+  def self.is_logged_in?(session)
+    session.has_key?(:user_id)
+    # s!!current_user(session)
+    # !! is just ! (the boolean negation operator) written twice. It will negate the argument, then negate the negation. It's useful because you can use it to get a boolean from any value. The first ! will convert the argument to a boolean, e.g. true if it's nil or false, and false otherwise. The second will negate that again so that you get the boolean value of the argument, false for nil or false, true for just about everything else.
+  end
 end
