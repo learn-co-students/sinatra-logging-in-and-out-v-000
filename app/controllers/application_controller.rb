f4175ceaa7da068f4ddbc77c89639f3c3991eabe
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-10 23:36:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
@@ -24,4 +35,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
