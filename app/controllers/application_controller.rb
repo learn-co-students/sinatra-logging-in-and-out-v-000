06b2f92584a38363417391471e2bc1dfea0975c0
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-10 23:49:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user
+    if @user && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
     else
@@ -30,7 +30,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
