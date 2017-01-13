32ba9f4e1a4552345ac6216a29a9163cb46f223e
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-13 11:38:54 -0500

    update helper sol

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class Helpers
 
-  def self.current_user(session_info)
-    @user = User.find_by_id(session_info["user_id"])
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
 
-  def self.is_logged_in?(session_info)
-    !!session_info["user_id"]
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
 end
