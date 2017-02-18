2724ccf2eb9f4d153f2d7a19ace4705777d68a96
Nicholas Ratti <nickratti@gmail.com>
2017-02-18 20:44:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,25 +14,27 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     # raise params.inspect
-    binding.pry
+    # binding.pry
     if User.find_by(username: params["username"]) != nil
       @user = User.find_by(username: params["username"])
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect '/account'
     else
-      redirect '/error'
+      erb :error
     end
   end
 
   get '/account' do
-    puts "This is the account page"
+    binding.pry
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
   get '/error' do
-    puts "This is the error page"
+    erb :error
   end
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+  def self.current_user(session)
+    # binding.pry
+    @user = User.find_by(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    # binding.pry
+    if session[:user_id] != nil
+      return true
+    else
+      return false
+    end
+  end
 end
