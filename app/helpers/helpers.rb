714e11f87e030913d2906adafd42efb9b0571047
vanita eckert <vanita112003@gmail.com>
2017-02-20 01:48:30 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,20 @@
 class Helpers
+
+  def self.current_user(session)
+    user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    # binding.pry
+    user = User.find_by_id(session[:user_id])
+     user.id == session[:user_id]
+     # if user
+    #   true
+    # else
+    #   false
+    # end
+  end
+
+
+
 end
