3da8e1166f7e697586b5f31a7cb1b97d57e0f9cc
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-13 19:28:13 -0400

    Working...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,42 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
-  configure do
-    set :views, Proc.new { File.join(root, "../views/") }
-    enable :sessions unless test?
-    set :session_secret, "secret"
-  end
+    configure do
+        set :views, Proc.new { File.join(root, "../views/") }
+        enable :sessions unless test?
+        set :session_secret, "secret"
+    end
 
-  get '/' do
-    erb :index
-  end
+    get '/' do
+        erb :index
+    end
 
-  post '/login' do
+    post '/login' do
+        @user = User.find_by(username: params[:username], password: params[:password])
 
-  end
 
-  get '/account' do
+        if @user
+            session[:user_id] = @user.id
+            redirect to '/account'
+        else
+            "Error" 
+        end
+    end
 
-  end
+    get '/account' do
 
-  get '/logout' do
+        @user = User.find(session[:user_id])
+        if @user
+            @session = session
+            erb :account
+        else
+            "Error" 
+        end
+    end
 
-  end
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
 
 
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+    
+    def self.current_user(session)
+#        binding.pry
+        id = session[:user_id]
+        User.find(id)
+    end
+    
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+    end
 end
