32dbe8f8b3bf3a23e2894b5e1e290ad541e92429
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-28 15:32:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,33 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    # User.create(username: "anytimesoon", password: "password", balance: 100.00)
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if !!@user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to('/account'), 302
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      # binding.pry
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to('/'), 302
   end
 
 
 end
-
