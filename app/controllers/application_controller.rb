2a96be6d4d65bc65e500ff809cfb7e9e4fca94e7
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-04-09 18:10:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,32 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    user = User.find_by(:username => params[:username])
+    if user != nil && user[:password] == params[:password]
+      session[:user_id] = user.id
+      redirect '/account'
+    else
+      erb :'/error'
+    end
   end
 
   get '/account' do
-
+    user = User.find_by(session[:user_id])
+    if user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
+  get '/error' do
+    erb :error
   end
 
 
 end
-
