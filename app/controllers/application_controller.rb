4326c471c85db5920abd7e1d7c96b51bc3840e53
github username <github email address>
2017-04-14 22:16:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,67 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do
+#   get '/' do
+#     erb :index
+#   end
+
+#   post '/login' do
+#     @user = User.find_by(username: params[:username])
+#     if @user 
+#     session[:user_id] = @user.id
+#     redirect to '/account'
+    
+#   end
+#     erb :error
+#   end
+
+
+#   get '/account' do
+#     current_user = User.find_by_id(session[:user_id])
+#     if @current_user
+#       erb :account
+#     else
+#       erb :error
+#     end
+#   end
+
+
+  
+
+#   get '/logout' do
+#     session.clear
+#     redirect to '/'
+
+#   end
+
+
+# end
+
+get '/' do
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
-
 end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+
+	def self.current_user(session_hash)
+		User.find(session_hash[:user_id])
+
+	end
+
+	def self.is_logged_in?(session_hash)
+		if session_hash[:user_id] 
+			return true
+		else
+			return false
+		end
+	end
 end
