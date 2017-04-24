635795f038a36e442b3f890b003d9b248c3e80ea
Erika Hughes <ehughes@knights.ucf.edu>
2017-04-24 00:59:49 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(session)
+    user = User.find(session[:user_id])
+    user
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
