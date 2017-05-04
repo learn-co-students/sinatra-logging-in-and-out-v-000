a3396d5779588bf9f71004624a45be87f766eb84
Feiran Wang <wangfeiranpku@163.com>
2017-05-04 02:16:42 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+  def self.current_user(session)
+    User.find(session[:user_id])
+
+  end
+
+  def self.is_logged_in?(session)
+    if self.current_user(session) == nil
+      false
+    else true
+    end 
+
+  end
 end
