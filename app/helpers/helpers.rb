11bcadb2e39b0e540bf9407eed82c09b22bad734
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-05 13:37:46 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
 class Helpers
+
+  def self.current_user(session)
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def self.is_logged_in?(session)
+   !!self.current_user(session)
+  end
+
 end
