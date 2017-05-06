9438345a1d71e181fd9d09f74380301e662d9337
nadinesk <nadine.khattak@gmail.com>
2017-05-05 22:05:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,47 +11,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    
 
     @user = User.find_by(username: params[:username])
 
     if @user   
-      session[:user_id] = @user.id      
-      
+      session[:user_id] = @user.id            
       redirect '/account'
     end
     
     erb :error
-    
-
 
   end
 
-  get '/sessions/logout' do 
-    
-    session.clear
-
-    redirect '/'
-  end
-
-  get '/error' do 
-
-    erb :error
-
-  end
-
-
+  
   get '/account' do
-    
-
-    
-    #if !session[:user_id]      
-      
-     # puts "no session"
-      #{}"You Must <a href="/">Log In</a> to View Your Balance"
-    #end
-
-
+   
       @user = Helpers.current_user(session) 
 
       if @user
@@ -60,27 +34,11 @@ class ApplicationController < Sinatra::Base
         erb :error
       end
 
-
-      #puts "Your Balance: #{'%.1f' % user.balance}"
-      
-#      if Helpers.is_logged_in(session) 
- #       puts "#{user.username}"
-  #      puts "#{user.balance}"
-   #     binding.pry
-    #  else 
-     #   puts "blah"
-      #  binding.pry
-      #end 
-
-      
-
-    
-
   end
 
   get '/logout' do
+    
     session.clear
-
     redirect '/'
 
   end
