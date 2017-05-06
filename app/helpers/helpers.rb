68601aeaa4e21e949849a088ae1fd9adcb6da4d0
nadinesk <nadine.khattak@gmail.com>
2017-05-05 22:03:04 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+	def self.current_user(session)
+		
+		@current_user = User.find_by_id(session[:user_id])
+		
+		
+
+	end
+
+	def self.is_logged_in?(session)
+		!!session
+		
+	end
+
 end
---
+++ db/migrate/20170505144451_users.rb
@@ -0,0 +1,10 @@
+class Users < ActiveRecord::Migration
+  def change
+  	create_table :users do |t|
+  		t.string :username
+  		t.string :password
+  		t.integer :balance, :scale =>1
+  	end
+
+  end
+end
