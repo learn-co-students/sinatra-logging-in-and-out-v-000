d988d057f0aa077132878586d4145c352573e90c
Jdville03 <jarrel.delottinville@gmail.com>
2017-05-12 21:49:50 -0400

    Refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @session = session
-    erb :account
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
