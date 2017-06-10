c57937f004f07d5115dae2b2aebb284e3c33ff70
Niki Lee <ew1415048nk@gmail.com>
2017-06-10 22:18:01 +0900

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
@@ -11,17 +12,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username:params[:username], password:params[:password])
+    if @user == nil
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
 
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
 end
-
