d0b47298b771cb7856bbdce1af8dd749dad52b9d
streamedLine <dovidlwtn@gmail.com>
2017-06-13 23:04:02 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < Sinatra::Base
     if user
       session[:user_id] = user.id
       redirect to :'/account'
+    else
+      erb :error
     end
-    erb :error
   end
 
   get '/account' do
