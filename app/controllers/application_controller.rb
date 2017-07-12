3dba976c7330919cc446a8774bc03c9039516e50
gb23 <gregbenj@hotmail.com>
2017-07-12 14:17:16 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params["username"])
+    if !@user.nil?
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
-
+    @session = session
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
