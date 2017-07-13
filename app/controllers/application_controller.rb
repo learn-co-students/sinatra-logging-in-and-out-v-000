c63452a375bcaee07054428ad66e0cb97d3f5a90
Noah Summers <summers.noah@gmail.com>
2017-07-13 12:52:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,34 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    if User.find_by(params)
+      session[:user_id] = User.find_by(params).id
+      redirect '/account'
+    elsif User.find_by(username: params[:username])
+      # password must be wrong
+      @error = "Incorrect password."
+      erb :error
+    else
+      @error = "User #{params[:username]} not found."
+      erb :error
+    end
 
   end
 
   get '/account' do
 
+    if Helpers::is_logged_in?(session)
+      erb :account
+    else
+      @error = "Please log in."
+      erb :error
+    end
+
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
