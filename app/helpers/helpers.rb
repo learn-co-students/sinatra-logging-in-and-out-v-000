0ec6b80df250317687c6600ac4d8cfdee834f698
Scott Wakeman <swakeman@ca.rr.com>
2017-07-24 00:39:08 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def current_user
+
+  end
+
+  def is_logged_in?
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
+class User
 
+ attr_accessor :id, :username, :password, :balance
 
+ def initialize(id, username, password, balance)
+   @id = id
+   @username = username
+   @password = password
+   @balance = balance
+ end
+
+ def self.create_table
+   sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS users (
+       id INTEGER PRIMARY KEY,
+       username TEXT,
+       password TEXT,
+       balance DECIMAL
+       )
+       SQL
+   DB[:conn].execute(sql)
+ end
+
+end
