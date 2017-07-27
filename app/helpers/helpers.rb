10094db4339a552140bb17498a046eebdcf90adc
charlie lee <ctlee9096@gmail.com>
2017-07-27 00:43:58 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,14 +2,14 @@ require 'pry'
 class Helpers
    def self.current_user(session)
     # binding.pry
-     @user_id = User.find_by(session[:user_id])
+     @user_id = User.find(session[:user_id])
      #binding.pry
      @user_id
    end
 
    def self.is_logged_in?(session)
      #binding.pry
-     @user_id2 = User.find_by(session[:user_id])
+     @user_id2 = User.find(session[:user_id])
      if @user_id2 == @user_id
        true
      else
