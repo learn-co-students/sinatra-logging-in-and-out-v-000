eaaf9d1ff652e311294c850ec868cce002abde75
Ryan Do <falcsmike7@gmail.com>
2017-08-05 19:47:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    if @user = User.find_by(username: params[:username])
+      if @user.password == params[:password]
+        session[:user_id] = @user.id
+        redirect to "/account"
+      else
+        erb :error
+      end
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if session[:user_id]
+      @session = session
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to "/"
   end
 
 
 end
-
