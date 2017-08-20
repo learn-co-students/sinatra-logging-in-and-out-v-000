8ffe74b07f8704c1ee28898471f97e960a9103a0
halfields <hfields697@aol.com>
2017-08-20 00:19:00 -0500

    done

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
 class Helpers
+
+	def self.current_user(session)
+		user_id = session[:user_id]
+		User.find(user_id)
+	end
+
+	def self.is_logged_in?(session)
+		!!session[:user_id]
+	end
 end
