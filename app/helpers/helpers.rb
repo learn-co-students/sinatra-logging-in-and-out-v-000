eb08cc74616c21e8b5b118a96e4433a7b12b5c1a
zoebisch <zoebisch@gmail.com>
2017-08-25 18:28:01 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
 
   def self.current_user(session_hash)
-    @user = User.find_by_id(session_hash[:user_id])
+    User.find_by_id(session_hash[:user_id])
   end
 
   def self.is_logged_in?(session_hash)
