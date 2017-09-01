de2d208df0b317537f7ea88b7f4e5a46f69e024b
jonathan <jonathanyi81@gmail.com>
2017-09-01 00:30:30 -0400

    trying to finish up lab, account is not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -11,17 +12,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"], password: params["password"])
+    #binding.pry
+    if @user == nil
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
   end
 
   get '/account' do
-
+    #@user = User.find(session[:user_id])
+    @session = session
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,8 @@
 class Helpers
+  def self.current_user(session)
+    user = User.find_by(session[:id])
+  end
+  def self.is_logged_in?(session)
+    !session.empty?
+  end
 end
