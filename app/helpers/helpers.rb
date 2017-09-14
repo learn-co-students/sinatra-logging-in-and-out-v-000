b9a1d1a20b2c2087b7a46dd9bb1e943a06d28485
Mariel F <marielbeth@gmail.com>
2017-09-14 00:57:07 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session)
+    User.find_by(id: session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if !!self.current_user(session)
+      session[:user_id] == self.current_user(session).id
+    else
+      false
+    end
+  end
 end
