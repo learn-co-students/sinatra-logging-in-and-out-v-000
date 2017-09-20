4ef50f924eb431709e6d9d72ea73a99c4d15fa6b
Cynthia Rittenbach <crittenbach@gmail.com>
2017-09-20 02:30:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
 
   post '/login' do
-    @user = User.find_by(username: params["username"])
+    @user = User.find_by(username: params[:username], password: params[:password])
     if @user == nil
       erb :error
     else
