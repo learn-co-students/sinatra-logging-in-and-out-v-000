88d0bb09d43f2cd4bb476c921a1aa536136531fa
Dominique McCloud <niquelejon@gmail.com>
2017-09-20 18:02:04 +0000

    completed

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+#!! forced string into boolean context (true) and then negated (false), and then negated again (true)
+  def self.is_logged_in?(session)
+    !!(session[:user_id])
+  end
 end
