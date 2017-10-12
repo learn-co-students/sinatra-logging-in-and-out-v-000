ff1195d7cdc31adb72c4ed2ef66706eef7a17709
Johan Esteves <jsesteves91@gmail.com>
2017-10-12 02:38:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
     erb :index
   end
 
