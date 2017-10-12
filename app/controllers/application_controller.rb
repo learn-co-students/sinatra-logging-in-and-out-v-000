ecebf85782fe21c86e1ad93c63614713c7e968e7
Johan Esteves <jsesteves91@gmail.com>
2017-10-12 02:31:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,16 +13,27 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
-    session[:id] = @user.id
-    @user != nil ? (redirect to '/account') : (erb :error)
+    if @user == nil
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
   end
 
   get '/account' do
-    erb :account
+    if session[:user_id]
+      @current_user = Helpers.current_user(session)
+      @logged_in = Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
     session.clear
+    redirect to '/'
   end
 
 
