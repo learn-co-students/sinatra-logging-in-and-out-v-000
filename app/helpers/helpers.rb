a2bd3837364eac55ca31e7dac97b430a3515df78
Christian Egglin <cegglin@conncoll.edu>
2017-10-16 20:02:21 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  def self.current_user(session)
+    if !!session[:user_id]
+      user_id = session[:user_id]
+      User.find(user_id)
+    end
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
