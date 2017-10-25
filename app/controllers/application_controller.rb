39467abca9c77bfcbfffaea6e6f9b9803c5e445a
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-25 17:23:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
+require 'pry'
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -10,18 +12,28 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  # Find user in db if @user.id exists, redirect to account page
   post '/login' do
-
+    if @user = User.find_by(params)
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
