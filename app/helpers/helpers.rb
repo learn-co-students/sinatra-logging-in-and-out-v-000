0b8b725dfd85e64b43cd9de421de5a20cbd6ae47
GusVilla303 <gusvilla303@gmail.com>
2017-10-28 13:11:10 -0600

    Add Helpers.current_user and Helpers.is_logged_in? to and pass specs.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,12 @@
+require 'pry'
+
 class Helpers
 
   def self.current_user(session)
-    user = User.find_by(username: session[:username],password: session[:password])
+    user = User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    current_user(session) == User.find(session[:user_id]) ? true : false
   end
 end
