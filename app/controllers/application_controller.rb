9eca5a3302ef1cce101301efc7fbec18d13437c7
GusVilla303 <gusvilla303@gmail.com>
2017-10-28 21:11:12 -0600

    Remove binding.pry from action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username], password: params[:password])
     if user
       session[:user_id] = user.id
-      # Helpers.current_user(session)
-      # binding.pry
+    
       redirect to '/account'
     else
       erb :error
