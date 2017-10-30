b6e0712e9c9d81d48959e36d5abc81bcb31ed80e
mandlerm <amaya.melissa@gmail.com>
2017-10-30 14:53:56 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user(hash)
+
+    @user = User.find_by(id: hash[:user_id])
+  end
+
+  def self.is_logged_in?(hash)
+
+    hash[:user_id] == session[:id]? true: false
+  end
 end
