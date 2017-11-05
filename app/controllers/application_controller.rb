01cddbc53fa51a6e016835b0aff11281508f160e
Denis Bondin <dsbondin@gmail.com>
2017-11-05 18:31:53 +0000

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,11 +21,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      'You Must <a href="/">Log In</a> to View Your Balance'
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
