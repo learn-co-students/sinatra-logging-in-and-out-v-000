dfff831f39e959c1a85f7c5c4a266f96576faf4b
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-11-11 02:40:01 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
-class Helpers
+class Helpers < Sinatra::Base
+  configure do
+    set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+  def self.current_user(session)
+    user = User.find_by(id: session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!self.current_user(session)
+  end
 end
