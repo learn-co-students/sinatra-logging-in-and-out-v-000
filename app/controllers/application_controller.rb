373b7a857bf1c965ff95970b0bb7519afe46a562
alpbourne <alpbourne@gmail.com>
2017-11-15 02:45:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user = nil && @user.password == params[:password])
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
+  end
+
 
   end
 
@@ -24,4 +33,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
