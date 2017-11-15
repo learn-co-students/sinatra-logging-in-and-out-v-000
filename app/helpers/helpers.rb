e266807a5b8eacde6a03823b11b84bc1652b5da4
Justin Tooley <jstooley823@gmail.com>
2017-11-15 02:10:21 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] != nil
+      !!User.find(session[:user_id])
+    end
+  end
 end
