492ad560ea1a79b6b4b2a42a0aa171c590de1de6
Mars Howard <marsbhoward@gmail.com>
2017-11-20 17:33:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,39 @@
 require_relative '../../config/environment'
-class ApplicationController < Sinatra::Base
-  configure do
-    set :views, Proc.new { File.join(root, "../views/") }
-    enable :sessions unless test?
-    set :session_secret, "secret"
+
+  class ApplicationController < Sinatra::Base
+    configure do
+      set :views, Proc.new { File.join(root, "../views/") }
+      enable :sessions unless test?
+      set :session_secret, "secret"
+    end
+
+    get '/' do
+      erb :index
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:username])
+      if @user.nil?
+        erb :error
+      else
+        session[:user_id] = @user.id
+        redirect to '/account'
+      end
+    end
+
+    get '/account' do
+      if Helpers.is_logged_in?(session)
+        @user = User.find_by(id: session[:user_id])
+        erb :account
+      else
+        erb :error
+      end
+    end
+
+    get '/logout' do
+      session.clear
+      redirect '/'
+    end
+
+
   end
-
-  get '/' do
-    erb :index
-  end
-
-  post '/login' do
-
-  end
-
-  get '/account' do
-
-  end
-
-  get '/logout' do
-
-  end
-
-
-end
-
