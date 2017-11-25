d9e453447c42350157fd130fe61a693960ab4adc
miriam <miriam5000@gmail.com>
2017-11-24 22:50:48 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ require 'sinatra'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    # set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
@@ -14,13 +16,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    #find based on their username 
-    # if match, 
+    
+    
     @user = User.find_by("username" => params[:username])
+     
     if @user != nil and @user.password == params[:password]
       sesssion[:user_id] = @user.id
       redirect to '/account'
       end 
+
     erb :error
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,8 +2,11 @@ require 'sinatra/base'
 
 class Helpers
 
+
 	def self.current_user(session)
+
 		@user = User.find_by_id(session[:user_id])
+
 
 	end 
 
