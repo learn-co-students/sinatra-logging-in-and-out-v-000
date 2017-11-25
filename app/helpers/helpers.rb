0b637058b4a0770048ff5111b08a4b92f516f595
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-25 21:52:57 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
 class Helpers
+
+  def self.current_user(session)
+    #binding.pry
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    session[:user_id] ? true : false
+  end
 end
