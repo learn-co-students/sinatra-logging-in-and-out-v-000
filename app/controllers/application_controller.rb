f858cfee2a8df1d95728ab9b8e368cb637bd6c22
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-11-26 14:24:34 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,16 +18,21 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/account'
    end
-
+   erb :error
   end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+        erb :account
+      else
+        erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
 
 end
