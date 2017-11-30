1125271fc52447da4889e4d9b419ac1cbe5c9064
Nicholas Halloran <nmhalloran@hotmail.com>
2017-11-30 21:38:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username])
 
   end
 
@@ -24,4 +25,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user
+    User.find(session[:id])
+
+  end
+
+  def self.is_logged_in?
+
+  end
+
 end
