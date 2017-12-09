d6f0f2a54f94f72edbb4ddb6848e94cf8c636be7
natgit8 <natasha.gitlin8@gmail.com>
2017-12-09 18:27:03 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -11,17 +10,27 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    if user = User.find_by(username: params["username"], password: params["password"])  #if user assignment
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
+
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
+
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
-
 end
-
