9cdf4bc43aac666074c036c5e9cd2b4d9822bdc3
dbfarms <matt.hunger@gmail.com>
2017-12-19 23:17:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    
     erb :index
   end
 
   post '/login' do
-
+     #binding.pry
+    
+    if User.find_by(:username => params[:username]) == nil 
+      #binding.pry
+      erb :error 
+    else 
+      @user = User.find_by(:username => params[:username])
+      
+      session[:user_id] = @user.id 
+      #binding.pry
+      redirect to '/account'
+    end 
   end
 
   get '/account' do
-
+      #binding.pry
+      if session[:user_id] == nil 
+        erb :error 
+      else 
+        @user = User.find(session[:user_id])
+        erb :account
+      end 
   end
 
   get '/logout' do
-
+    session.clear 
+    redirect to '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,20 @@
+require 'sinatra/base' 
+
 class Helpers
+  
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+    @user 
+  end 
+  
+  def self.is_logged_in?(session)
+    
+    if session[:user_id]
+      #binding.pry
+      return true 
+    else 
+      #binding.pry
+      return false 
+    end 
+  end 
 end
