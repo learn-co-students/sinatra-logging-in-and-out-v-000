a7f4d8c98e5de313927b2025fd62f844c6c72241
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-12-21 06:31:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -11,17 +12,27 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.all.find_by(username: params[:username])
+    if @user.is_a?(User)
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if  Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      binding.pry
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
-
