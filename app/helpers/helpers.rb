8977416e40c7a325dba8c1d975f65d985f97f838
Lauren Feld <lauren.feld@aol.com>
2018-01-04 14:58:14 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  def self.current_user(session)
+    @id = session[:user_id]
+    @user = User.find(@id)
 end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] != nil
+      true
+    end
+  end
+end 
