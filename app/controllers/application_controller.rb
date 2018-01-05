22506e57f63d5f2f066ebefd67f11617ee0e7dbb
Lara Sanders <larasanders@gmail.com>
2018-01-05 01:01:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,33 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+  # binding.pry
 
+    #  @user = User.create(username: params["username"], password: params["password"], balance: params["balance"])
+    @user.find_by(username: params["username"])
+    if @user != nil
+    session[:user_id] = @user.id
+    redirect to '/account'
+    binding.pry
+  else
+     erb :error
   end
+end
 
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user =! nil
+      binding.pry
+      erb :account
+    else
+       erb :error
   end
+end
 
   get '/logout' do
-
+    session.clear
+  redirect '/'
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
+require 'pry'
 class Helpers
+
+  def self.current_user(session)
+    # binding.pry
+    # @user = User.create(username: params["username"], password: params["password"], balance: params["balance"])
+    # session = session[:id]
+    @user = User.find_by_id(session[:user_id])
+      # session[:user_id] = @user.id
+      # @user
+end
+
+def self.is_logged_in?(session)
+  !!session[:user_id]
+# binding.pry
+end
 end
