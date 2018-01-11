68600bc142f16abf01725541c82e7631ccc0e9be
Steven Senkowski <scsenkowski@gmail.com>
2018-01-11 17:27:58 +0000

    Done.

---
+++ db/migrate/20180111164408_users.rb
@@ -0,0 +1,9 @@
+class Users < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |c|
+      c.string :username
+      c.string :password
+      c.decimal :balance
+    end
+  end
+end
