9eb517ff106a295fd8696820149154240ed1d8ae
Hannah Carnes <hecarnes@gmail.com>
2018-01-13 11:56:35 -0500

    improvements: refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user != nil && @user.password == params[:password]
+    @user = User.find_by_username(params[:username])
+    if @user && @user.password == params[:password]
       session[:user_id] = @user.id
-      redirect to '/account'
+      redirect '/account'
     else
-      erb :error
+      @error = "Invalid username or password"
+      erb :index
     end
   end
 
@@ -31,7 +32,7 @@ class ApplicationController < Sinatra::Base
 
   get '/logout' do
     session.clear
-    redirect to '/'
+    redirect '/'
   end
 
 end
