741c9a442d392bca2fa0b0706205d9e489d08fa7
github username <github email address>
2018-01-17 12:30:24 -0600

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,7 +4,10 @@ class Helpers
         @user = User.find_by(session[:user_id])
     end
 
+    # How does class Helpers have access to @user?
+
     def self.is_logged_in?(session)
         session.include?(:user_id)
+        # !!session[:user_id]
     end
 end
