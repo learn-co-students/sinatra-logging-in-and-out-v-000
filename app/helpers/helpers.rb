b1c917834195c14bd3ec9fb906a43b6a3908a51d
Sherry El Garhy <sherryptk@gmail.com>
2018-01-20 15:14:38 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,22 +1,18 @@
 require 'pry'
-class Helpers <
+class Helpers
 
   def self.current_user(session)
-    User.all.find(session[:user_id])
+    session.collect do |key, value|
+      User.all.find(value)
+    end
   end
 
-  # current_user should accept the session hash as an argument.
-  # This method should use the user_id from the session hash to find the user in the database and return that user.
-
   def self.is_logged_in?(session)
-    if session.include(user.id)
+    if session.include?(:user_id)
       return true
     else
       return false
     end
   end
-  # is_logged_in? should also accept the session hash as an argument.
-  # This method should return true if the user_id is in the session hash and false if not.
-  # The Ruby !! operator will come in handy here.
 
 end
