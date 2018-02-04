17d04588abbb7e20d28e5443c5adb1a0239f63a7
Parker Catalano <catalano.work@gmail.com>
2018-02-04 19:14:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if @user = User.find_by_id(session[:user_id])
+    if @user = User.find_by(id: session[:user_id])
       erb :account
     else
       erb :error
