cfa8fd4fb06b2306ed27de585ebab27b4c1372f9
Sally Zasloff <szasloff@gmail.com>
2018-02-18 19:11:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
-      # binding.pry
+
       redirect to '/account'
 
     else
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
   end
 
   def self.is_logged_in?(session)
