e94e581dbe23d5bceb8ddc8c69d2b51d76611f4f
funemployeddad <funemployeddad@gmail.com>
2018-02-21 10:59:26 -0700

    Done.

--- db/migrate/20180220211752_create_users.rb
+++ db/migrate/20180220211752_create_users.rb
@@ -1,9 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+class CreateUsers < ActiveRecord::Migration[4.2]
+
+def change
     create_table :users do |t|
-      t.string = :username
-      t.string = :password
-      t.decimal = :balance
+      t.string :username
+      t.string :password
+      t.decimal :balance
     end
   end
 end
