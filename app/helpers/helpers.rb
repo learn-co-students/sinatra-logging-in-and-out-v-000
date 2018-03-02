12c73725a78cabb3a78c445c6e7e8885b3be3caa
Katrina Brinson <kbrinso@gmail.com>
2018-03-02 20:51:05 +0000

    Helpers Class Complete

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+  #these class methods can only be called in account.erb
+  def self.current_user(session)
+    #binding.pry
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if User.find(session[:user_id]) != nil
+      return true
+    else
+      return false
+    end
+  end
 end
