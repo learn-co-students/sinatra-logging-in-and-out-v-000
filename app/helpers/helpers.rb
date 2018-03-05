674e3aae0a39ad90085411fb8bef23e6930e803e
Alex Crisan <alexcrisan2005@hotmail.com>
2018-03-05 20:34:53 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+  def self.current_user(session)
+    @current_user = User.find(session[:user_id])
+    @current_user
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id]
+      return true
+    else
+      return false
+    end
+  end
+
 end
