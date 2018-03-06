966663121e3cefaaf936c2be1201a3ffbba3245f
Jerry Boyce <jokerdelta@yahoo.com>
2018-03-06 05:07:01 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session)
+      User.find_by(id: session[:user_id])
+  end
+
+  def self.is_logged_in?(session)#return true if the user_id is in the session hash and false if not
+    session.has_key?(:user_id)
+  end
+
 end
