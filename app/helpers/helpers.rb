d8a9b3e0e00566acaf05e161fbdd440b22ed6350
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-17 08:26:54 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
+# Provides helper methods
 class Helpers
+  def self.current_user(params)
+    User.find_by(id: params[:user_id])
+  end
+
+  def self.is_logged_in?(params)
+    params.key?(:user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
-
-
+# User model
+class User < ActiveRecord::Base
+  validates_presence_of :username, :password, :balance
+end
