5c181df4b9d89dbbb48f021f6fae7a34ab55c025
Chris Henry <cdhenry@gmail.com>
2018-03-17 15:48:30 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if !!(session[:user_id])
+      true
+    else
+      false
+    end
+  end
 end
