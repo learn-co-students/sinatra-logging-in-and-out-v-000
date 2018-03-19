60c7d6f5811a2da7d829a63ab348b431af85be4f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-19 16:20:47 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,5 +4,11 @@ class Helpers < ActiveRecord::Base
     user
   end
   def self.is_logged_in?(session)
+    user= User.find(session[:user_id])
+    if user
+      return true
+    else
+      return false
+    end
   end
 end
