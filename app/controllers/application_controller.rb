8005c5d3ad4209cafe01f986fc4d63b3c12ef38c
Kevin Huang <yellow.khaos@gmail.com>
2018-03-25 20:52:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,15 +15,23 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect '/account'
+    else
+      erb :error
     end
   end
 
   get '/account' do
-    erb :account
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
