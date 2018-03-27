536d04ff4af775319a0e92e78d9eec08228ee976
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-27 19:20:26 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,9 +7,15 @@ class Helpers
   end
 
   def self.is_logged_in?(session)
-    @user = User.find_by(id: session[:user_id])
-    if @user
-      @user.id == session[:user_id] ? true : false
-    end
+    !!session[:user_id]
+  end
+
+end
+=begin
+def self.is_logged_in?(session)
+  @user = User.find_by(id: session[:user_id])
+  if @user
+    @user.id == session[:user_id] ? true : false
   end
 end
+=end
