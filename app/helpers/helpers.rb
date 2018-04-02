feccd58359968e1b874b0c8bc551aba23e8e672e
Erin Walker <erinb0317@hotmail.com>
2018-04-02 23:54:03 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+
+  def self.current_user(session_hash)
+    @current_user = User.find_by(id: session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
 end
