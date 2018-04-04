8af88c04a257e6821072f81641e5741d5d145624
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-04 17:48:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,20 +15,22 @@ class ApplicationController < Sinatra::Base
     if @user == nil
       erb :error
     else
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect '/account'
     end
   end
 
   get '/account' do
-    @user = User.find_by(id: session[:id])
-    erb :account
+    if session[:user_id] == nil
+      erb :error
+    else
+      erb :account
+    end
   end
 
   get '/logout' do
     session.clear
     redirect '/'
   end
-
 
 end
