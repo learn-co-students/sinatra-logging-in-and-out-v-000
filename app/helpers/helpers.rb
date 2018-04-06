4d907dd914aad4b2b9cccc148f0ef43edcbfad14
EC2 Default User <ec2-user@ip-172-31-14-103.us-east-2.compute.internal>
2018-04-06 14:35:11 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+    def self.current_user(session)
+        # binding.pry
+        @user = User.find(session[:user_id])
+    end
+    
+    def self.is_logged_in?(session)
+        @user = User.find_by(id: session[:user_id])
+        !(@user == nil)
+    end
 end
