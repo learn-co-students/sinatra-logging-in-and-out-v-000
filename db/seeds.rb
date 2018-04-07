f82e26357a0c7c9f26f66d15c42dafdb78a4ad42
smithwebtek <brad@smithwebtek.com>
2018-04-06 21:34:58 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ user1 = User.create(:username => "skittles123", :password => "iluvskittles", :ba
 user2 = User.create(:username => "flatiron4lyfe", :password => "Rubie!", :balance => 500)
 
 user3 = User.create(:username => "kittens1265", :password => "crazycatlady", :balance => 10000)
+
+user4 = User.create(:username => "smithpiano", :password => "asdf", :balance  => 1000)
+ 
+user5 = User.create(:username => "fred", :password => "flintstone", :balance  => 13)
