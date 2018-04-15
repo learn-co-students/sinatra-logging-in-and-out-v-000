2ecf9f8bc4a423056660a5e07a8f321c7573138a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-15 20:29:59 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,6 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
-end
 
 ActiveRecord::Base.logger = nil
 
