4accff20fd51ea8ad59f0dbbd7c4acd530bd03d5
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-19 14:41:17 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,20 @@
 class Helpers
+  
+  def self.current_user(session)
+    if session.key?(:user_id)
+      @user = User.find(session[:user_id])
+      @user
+    else
+      nil
+    end
+  end
+  
+  def self.is_logged_in?(session)
+    if self.current_user(session)
+      session[:user_id]==self.current_user(session).id
+    else
+      nil
+    end
+   #binding.pry
+  end
 end
