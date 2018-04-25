743af87e978ef5c7ee84efd2c604d42c85afaf2e
John O'Connor <oconnojb@gmail.com>
2018-04-25 16:25:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,32 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"], password: params["password"])
+    if @user == nil
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
   end
 
   get '/account' do
-
+    if !!session[:user_id]
+      @user = User.find(session[:user_id])
+      if session[:user_id] = @user.id
+        erb :account
+      else
+        erb :error
+      end
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,22 @@
 class Helpers
+  def self.current_user(session)
+    # current_user should accept the session hash as an argument.
+    # This method should use the user_id from the session hash
+    # to find the user in the database and return that user.
+
+    user = User.find(session[:user_id])
+    return user
+  end
+
+  def self.is_logged_in?(session)
+    # is_logged_in? should also accept the session hash as an argument.
+    # This method should return true if the user_id is in the session hash
+    # and false if not. The Ruby !! operator will come in handy here.
+
+    if !!session[:user_id]
+      true
+    else
+      false
+    end
+  end
 end
