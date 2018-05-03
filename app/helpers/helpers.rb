8dc170b522f4cf24d4682d9e3cd5a02c7699f1e4
Joanne Lee <vernapii@gmail.com>
2018-05-03 22:44:08 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
-class Helpers
-end
+
+  class Helpers
+    def self.current_user(session)
+      User.find_by_id(session[:user_id])
+      # should it be session[:session_id] OR session[:id] ???
+    end
+
+    def self.is_logged_in?(session)
+      !!current_user(session)
+    end
+  end
