0e6c0dd16704ecb1f7419a896a2dce75252e79d9
bethurban <beth.urban@gmail.com>
2018-05-09 16:54:58 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+  def self.current_user(session)
+    @user_id = session[:user_id]
+    user = User.find_by(id: @user_id)
+    user
+  end
+
+  def self.is_logged_in?(session)
+    if session.key?(:user_id)
+      true
+    else
+      false
+    end
+
+  end
 end
