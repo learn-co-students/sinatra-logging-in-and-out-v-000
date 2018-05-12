23680fb89fde7557072e028a0c82eb039d1831af
Augusto Granitto <augustogranitto@gmail.com>
2018-05-12 22:31:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,9 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @current_user = User.find_by_id[session[:user_id]]
+    binding.pry
     if @current_user
+
       erb :account
     else
       erb :error
