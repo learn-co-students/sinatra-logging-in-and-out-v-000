cad70dbd4a2c7d586537fc1582303fb35fb3e5c3
Augusto Granitto <augustogranitto@gmail.com>
2018-05-12 21:33:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    #binding.pry
     if User.find_by(username: params[:username], password: params[:password])
       @user = User.find_by(username: params[:username], password: params[:password])
       session[:user_id] = @user.id
