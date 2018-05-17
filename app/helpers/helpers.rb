ca293de3f314b0339f07aa953d2dcfcf7b4a7c7e
David Ritchey <ritchey0713@gmail.com>
2018-05-17 01:03:15 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find(session[:user_id]) if session[:user_id] != nil
+  end
+
+  def self.is_logged_in?(session)
+    !!current_user(session)
+  end
 end
