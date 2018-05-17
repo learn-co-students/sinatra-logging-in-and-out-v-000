65dc8c33cd06f7bed28214d688af4ca0e9555087
Zac Willmington <zac.willmington@gmail.com>
2018-05-17 17:03:36 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+    def self.current_user
+        binding.pry
+        @user = User.find(session[:id])
+    end
+
+    def self.logged_in?
+        binding.pry
+        if current_user.id == session[:id]
+            true
+        else
+            false
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+class User < ActiveRecord::Base
+    
+    attr_accessor :username, :password, :balance
 
+    def initialize(username, password, balance)
+        @username = username
+        @password = password
+        @balance = balance
+    end
 
+end
