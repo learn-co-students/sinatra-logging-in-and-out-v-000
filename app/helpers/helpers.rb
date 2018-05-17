3c9bd60d64eaccaee9639e5642c53e98d228030c
Trevor Seitz <trevinla@gmail.com>
2018-05-17 20:29:10 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+    # binding.pry
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
