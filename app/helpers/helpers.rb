e3667c9a8c8f45c082df88a3fb0f2c2fa1a7146a
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-21 21:03:31 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  
+  def current_user (session_hash)
+    Users.find (session_hash[:user_id]
+  end 
+
+  def is_logged_in? (session_hash)
+    !! session_hash.[:user_id] 
+  end
+
 end
