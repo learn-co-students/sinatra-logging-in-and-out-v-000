9a125232246492629e2b6be44e284e6e1ab85c70
Neil Seejoor <nvseejoor@gmail.com>
2018-05-24 17:36:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,11 +22,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
