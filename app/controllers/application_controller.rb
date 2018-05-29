1547d00d6b7550e706abf98b90bf75f4aad8ae04
Victoria Meng <nights1ststar@protonmail.com>
2018-05-29 22:06:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    username = params['username']
+    password = params['password']
+    @user = User.find_by(username: username, password: password)
+    if @user
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to "/"
   end
 
 
 end
-
