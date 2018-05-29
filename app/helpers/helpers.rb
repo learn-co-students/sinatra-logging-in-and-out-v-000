0d58313513b9faa0784b18d9e43bf0a312190f77
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-29 21:17:16 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
   def self.current_user(session)
-    user = User.find(id: session[:user_id])
+    user = User.find_by(id: session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    if session[:user_id] != nil && User.find(id: session[:id])
+    if session[:user_id] != nil && User.find_by(id: session[:id])
       true
     else
       false
