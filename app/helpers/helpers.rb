4a5dddd8ac0f06440abb706e91fdc4043cea2ecc
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-29 20:22:46 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def current_user(session)
+    user = User.find(id: session[:user_id])
+  end
+
+  def is_logged_in?(session)
+    if session[:user_id] != nil && User.find(id: session[:id])
+      true
+    else
+      false
+    end
+  end
 end
