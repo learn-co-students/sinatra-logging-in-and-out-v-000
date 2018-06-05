9d0e039ec7ae1f6e30a7d213f149f1e5bcccff35
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-06-05 18:12:02 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,21 @@
 class Helpers
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
+  def self.log_in(username, password, session)
+    if user = User.find_by(username: username, password: password)
+      session[:user_id] = user.id
+    else
+      false
+    end
+  end
+
+  def self.log_out(session)
+    session.clear
+  end
 end
