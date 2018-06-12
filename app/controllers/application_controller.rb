ec76e51e706f53f5f97321b0c0c02fc6b19954eb
Yina Zhu <yinagit@gmail.com>
2018-06-12 03:11:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username=> params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
@@ -24,4 +29,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,10 @@
 class Helpers
 
-  def current_user
+  def current_user(sessions)
+    @user = User.find_by_id(sessions[:user_id])
   end
 
-  def is_logged_in?
+  def is_logged_in?(sessions)
+    !!session[:user_id] #a double bang returns either true or false
   end
 end
