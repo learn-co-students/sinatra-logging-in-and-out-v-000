50a093cc9517fb5dcede53f7aa93f1c6ec4ca613
Yina Zhu <yinagit@gmail.com>
2018-06-12 17:25:20 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,12 @@
+require 'sinatra/base'
+
 class Helpers
 
-  def current_user(sessions)
-    @user = User.find_by_id(sessions[:user_id])
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
   end
 
-  def is_logged_in?(sessions)
+  def self.is_logged_in?(session)
     !!session[:user_id] #a double bang returns either true or false
   end
 end
