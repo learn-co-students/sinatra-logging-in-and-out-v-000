ec25b97e6564336ebd4fb8e0166f97f8d686d5e8
Nicholas Powers <njpowers7915@gmail.com>
2018-06-15 17:49:17 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
+require 'pry'
+
+
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        @user = User.find_by(id: session[:user_id])
+        !@user.nil?
+    end
 end
