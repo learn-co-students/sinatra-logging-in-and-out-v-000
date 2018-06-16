a59b60fcf620428b343d08d4dca5236fd4535bc8
Lucy Wall <lhwall@gmail.com>
2018-06-16 12:51:40 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+  def self.current_user(session)
+  #binding.pry
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id]
+      true
+    end
+  end
+
 end
