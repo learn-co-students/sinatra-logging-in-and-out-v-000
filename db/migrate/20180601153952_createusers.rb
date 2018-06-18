aac9aa75e772ea55c648f592dcf67d0417007425
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-06-18 17:35:51 +0000

    Done.

---
+++ db/migrate/20180601153952_createusers.rb
@@ -0,0 +1,10 @@
+class Createusers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
+
+  end
+end
