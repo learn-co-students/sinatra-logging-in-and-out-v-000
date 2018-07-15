5d620a5761de60eed952da46556a109bef765eed
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-15 20:12:39 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+  def self.current_user(arg)
+    @user = User.find(arg[:id])
+
+  end
+
+  def self.is_logged_in?(arg)
+    @user = User.find(arg[:id])
+    !!@user
+  end
+  
 end
