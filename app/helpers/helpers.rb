aca6540bfdfa1d5f3b68d0a1daf0a5afd7ffe523
Rebecca Tong <rtong@buffalo.edu>
2018-07-18 18:41:57 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if self.current_user(session) == self.current_user(session)
+      return true
+    else 
+      false
+    end
+  end
+
 end
