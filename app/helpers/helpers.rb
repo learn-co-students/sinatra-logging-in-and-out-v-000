afdc5a3c8446129adffa371e31d8fc749897cc68
ERIC KELLY <eakelly220@gmail.com>
2018-07-19 00:49:44 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def self.is_logged_in?(session)
+    if @user = User.find(session[:user_id])
+       true
+    else
+       false
+    end
+  end
+
+
 end
