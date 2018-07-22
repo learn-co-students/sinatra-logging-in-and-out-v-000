e6f2d03e7e2fab58547d4b27d28d06e197e520e9
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-22 23:09:56 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
-class Helpers
+class Helpers < ActiveRecord::Base
+
+  def self.current_user(session)
+    User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    
+    if User.find_by_id(session[:user_id]) == nil
+      false
+    else
+      true
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,2 @@
-
+class User < ActiveRecord::Base
+end
