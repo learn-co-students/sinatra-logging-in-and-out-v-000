fa30ce3d7607ca964bcabd5725e24f7d4c808579
Ron Sala <ron.sala@gmail.com>
2018-07-27 13:51:32 +0000

    Done.

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsersTable < ActiveRecord::Migration [5.1]
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
 
 end
