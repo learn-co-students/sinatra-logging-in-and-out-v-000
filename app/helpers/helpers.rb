56caa63ea09b951f4e4fff9b905f70433cbfd806
Ron Sala <ron.sala@gmail.com>
2018-08-05 05:29:54 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,10 +2,27 @@ class Helpers
 
   def self.current_user(session)
     @user = User.find_by(id: session[:user_id])
+    # binding.pry
+#     [1] pry(Helpers)> @user
+# => #<User:0x000000040de5b8
+#  id: 1,
+#  username: "skittles123",
+#  password: "iluvskittles",
+#  balance: #<BigDecimal:1d6b310,'0.1E4',9(27)>>
+# binding.pry
+# [1] pry(Helpers)> session
+# => {:user_id=>1, :username=>"skittles123", :password=>"iluvskittles"}
   end
 
   def self.is_logged_in?(session)
     @user = User.find_by(id: session[:user_id])
     !!@user
+    # binding.pry
+    #  id: 1,
+ # username: "skittles123",
+ # password: "iluvskittles",
+ # balance: #<BigDecimal:1802478,'0.1E4',9(27)>>
+#  [2] pry(Helpers)> !!@user
+# => true
   end
 end
