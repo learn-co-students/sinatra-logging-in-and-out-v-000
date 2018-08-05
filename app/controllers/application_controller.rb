d54aeb01be5de35e67e371a898d3d9afdeb79e1b
Ron Sala <ron.sala@gmail.com>
2018-08-05 04:07:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,12 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user
+    if @user && @user.password == params[:password]
       session[:user_id] = @user.id
-#       binding.pry
-#       [1] pry(#<ApplicationController>)> params
-# => {"username"=>"skittles123", "password"=>"iluvskittles"}
-# [2] pry(#<ApplicationController>)> @user
-# => #<User:0x007f9a67351a70 id: 1, username: "skittles123", password: "iluvskittles", balance: #<BigDecimal:7f9a66caca08,'0.1E4',9(27)>>
-# [3] pry(#<ApplicationController>)> @user.id
-# => 1
-# [4] pry(#<ApplicationController>)> session
-# => {"session_id"=>"03d6ffd62c1118456071fe5d2ea2e43229787175f4762c75bae42c3e241506bc", "csrf"=>"PKE+bfaaIniS409TdLPWdagNNeiq67H7nfLbjcSHTEA=", "tracking"=>{"HTTP_USER_AGENT"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709", "HTTP_ACCEPT_LANGUAGE"=>"da39a3ee5e6b4b0d3255bfef95601890afd80709"}, "user_id"=>1}
       redirect to '/account'
+    else
+      redirect to '/error'
     end
-    redirect to '/error'
   end
 
   get '/error' do
@@ -34,7 +26,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    erb :account
+    @current_user = User.find_by_id(session[user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,20 +2,10 @@ class Helpers
 
   def self.current_user(session)
     @user = User.find_by(id: session[:user_id])
-    # binding.pry
-    # [1] pry(Helpers)> @user
-    # => #<User:0x007fdc435b6e50 id: 1, username: "skittles123", password: "iluvskittles", balance: #<BigDecimal:7fdc4329fe60,'0.1E4',9(27)>>
-    # [2] pry(Helpers)> session
-    # => {:user_id=>1}
   end
 
   def self.is_logged_in?(session)
     @user = User.find_by(id: session[:user_id])
     !!@user
-#     binding.pry
-#     [1] pry(Helpers)> @user
-# => #<User:0x007fb8e4a83690 id: 1, username: "skittles123", password: "iluvskittles", balance: #<BigDecimal:7fb8e3824d50,'0.1E4',9(27)>>
-# [2] pry(Helpers)> !!@user
-# => true
   end
 end
