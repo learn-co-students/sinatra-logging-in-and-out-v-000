198f0b61e6e3d738b787b31c71ab343067bee5c8
treverandersen <trever.r.andersen@gmail.com>
2018-08-06 12:39:47 -0500

    if matched--set session to the user's ID and redirects to /account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id 
+      redirect to '/account'
+    end
+    
   end
 
   get '/account' do
