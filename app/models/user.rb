6829cca0f1e69ba091eb771f26b4fdde7b8f7501
geoffrey woodcock <theatomproject@gmail.com>
2018-08-09 20:20:29 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
-
+class User < ActiveRecord::Base
+  
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.integer :balance
+    end
+  end
+  
+  
+end
 
