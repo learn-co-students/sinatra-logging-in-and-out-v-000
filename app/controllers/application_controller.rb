3908bf2aacbde7e8304eec48766beea189be4f86
Brent Bauman <brentbauman86@gmail.com>
2018-08-24 19:14:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,29 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+  erb :index
+end
+
+post '/login' do
+  user = User.find_by(username: params["username"])
+
+  if user
+    session[:user_id] = user.id
+    redirect to '/account'
+    erb :account
+  else
+    erb :error
   end
+end
 
-  post '/login' do
+get '/account' do
+  erb :account
+end
 
-  end
-
-  get '/account' do
-
-  end
-
-  get '/logout' do
-
-  end
+get '/logout' do
+  session.clear
+  redirect to '/'
+end
 
 
 end
-
