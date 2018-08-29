d4d7ae3c95b22e7ba96e00b5c125000e403cadb7
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-08-29 18:40:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    #binding.pry
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
