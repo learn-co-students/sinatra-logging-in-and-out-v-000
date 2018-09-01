2c0b96cc5a7e522be2ed90ac583967bf956ec731
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-01 16:10:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,19 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    if @user 
+   @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
-      redirect '/account'
-    else 
-      erb :error 
-  end
+      redirect to '/account'
+    end
+    erb :error
 end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
       erb :account
-    else 
+    else
       erb :error
     end
   end
