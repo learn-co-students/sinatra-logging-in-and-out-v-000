cce060162b4a8f22c2c6ba7b48dfbd6e1cf9e1cb
Christiana Vandermale <christianavdm@gmail.com>
2018-09-07 00:57:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
-    session[:user_id] = @user.id
     if @user != nil
+      session[:user_id] = @user.id
       redirect to '/account'
     else
       redirect '/error'
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    @user = User.find_by(id: session[:user_id])
     erb :account
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Helpers
-  def self.current_user(params)
-    @user = User.find_by(id: params[:user_id])
+  def self.current_user(session)
+    @user = User.find_by(id: session[:user_id])
   end
 
-  def self.is_logged_in?(params)
-    !!params[:user_id]
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
 
 end
