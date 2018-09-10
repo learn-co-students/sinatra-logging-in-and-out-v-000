43cf55459cb1f557bd494d86c773a209b20c2b37
Jonathan Chan <johnkchan252@gmail.com>
2018-09-10 03:57:10 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  
+  def self.current_user(session)
+    user = User.find_by(id: session[:user_id])
+    user
+  end
+  
+  def self.is_logged_in?(session)
+    session[:user_id] !=nil
+  end
+  
 end
