fe82f52890130a2c18e6a040bae75bd060d8fc88
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-14 14:14:50 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,15 +3,13 @@ require 'sinatra/base'
 class Helpers
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
-    @user.save
-    @user
   end
 
   # def self.current_user_username(session)
   #   @user = User.find_by_id(session[:user_id])
   #   @user.username
   # end
-  # 
+  #
   # def self.current_user_balance(session)
   #   @user = User.find_by_id(session[:user_id])
   #   @user.balance.to_f
