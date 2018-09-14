17bfa1c0565d029b09dd597552833c4a1e16da4c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-14 14:23:12 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Helpers
   def self.current_user(session)
+    binding.pry
     @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    # User.find_by(session[:user_id]) ? true : false
+    !!session[:user_id]
   end
 
   # def self.current_user_username(session)
@@ -14,9 +20,4 @@ class Helpers
   #   @user = User.find_by_id(session[:user_id])
   #   @user.balance.to_f
   # end
-
-  def self.is_logged_in?(session)
-    # User.find_by(session[:user_id]) ? true : false
-    !!session[:user_id]
-  end
 end
