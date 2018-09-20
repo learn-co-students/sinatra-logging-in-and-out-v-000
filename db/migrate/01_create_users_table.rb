c8f1162d51bbfdaa806db2b3dd38f8be681dce4d
Madeline Stark <madeline.stark1@gmail.com>
2018-09-20 15:51:05 +0000

    Done.

---
+++ db/migrate/01_create_users_table.rb
@@ -0,0 +1,9 @@
+class CreateUsersTable < ActiveRecord::Migration[4.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
+  end
+end
