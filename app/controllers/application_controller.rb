3f75421bb1e91ce7d391715c2209d280193948f0
Mary Jack <maryjackinthebox@gmail.com>
2018-09-30 23:31:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+  if !!@user = User.find_by(username: params["username"])
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if !session[:user_id]
+      erb :error
+    else
+      @user = Helpers.current_user(session)
+      erb :account
+    end
   end
 
   get '/logout' do
-
+  session.clear
+    redirect '/'
   end
 
 
