523523cbd4fb7b0e1df6a0fec992cfe1fa0f0aca
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-02 02:59:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
