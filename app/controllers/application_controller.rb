fb8ed13788417a2d5ffd6acc4c9a260c1b73b78a
Nathan  Visak <nathan.visak@gmail.com>
2018-10-12 21:29:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    if @user
       erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
     session.clear
+    redirect '/'
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008160507) do
+ActiveRecord::Schema.define(version: 20181012212422) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.integer "balance"
+    t.decimal "balance"
   end
 
 end
