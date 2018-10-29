849b404619efdf037b92646beea7c1093c7a4948
Srija S <srijakumar7@gmail.com>
2018-10-29 02:54:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -11,6 +12,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    binding.pry
     @user = User.find_by(params[:username])
 
     if @user.password == params[:password]
