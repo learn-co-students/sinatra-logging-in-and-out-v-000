3b025ea9c9dff83ebd3c8a288a4a33f53e2ea400
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-01 21:01:34 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,8 @@
 class Helpers
 
   def current_user(session)
-    @user = User.find_by(id: session[:user_id])
-    @user
+    @current_user = User.find_by(id: session[:user_id])
+    binding.pry
+    @current_user
   end
 end
