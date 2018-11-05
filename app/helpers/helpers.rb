6c713d0e488c353ec34c1cfa733b0ec9159b521c
Justine B <justinegayton@gmail.com>
2018-11-05 20:48:03 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,19 @@
-class Helpers
+require 'pry'
+
+class Helpers < ApplicationController
+
+  def self.is_logged_in?(session)
+  #  if session[:usernam].empty?
+  #    redirect "/index"
+    !!session[:user_id]
+  end
+
+  def self.current_user(session)
+    @user = User.find_by(:id => session[:user_id]) if session[:user_id]
+  end
+
+  def logout
+    session.clear
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
+require 'pry'
 
-
+class User < ActiveRecord::Base
+end
