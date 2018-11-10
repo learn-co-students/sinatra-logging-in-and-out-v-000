5dc835fcdeaee648526cb55c6fc912255b42e9ee
Sushi Chanrai <sushi@pathworks.co>
2018-11-09 19:14:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if !!@user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+      erb :error
   end
 
-  get '/account' do
 
+  get '/account' do
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
 end
-
