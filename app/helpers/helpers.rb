936008e46e05b9b96fd76357b98da9cbf7adb19c
Barbara Choy <barbchoy@gmail.com>
2018-11-14 03:33:28 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'sinatra/base'
+
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user_id])
+    @user
+  end
+
+  def self.is_logged_in?(session)
+    self.current_user(session) != nil
+  end
 end
