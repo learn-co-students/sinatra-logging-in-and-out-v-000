345d0400686da49c64e37a91bdddc1fb489b0601
Wendy Calderon <wendycalderon@gmail.com>
2018-12-11 03:12:38 +0000

    Set /login in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if !!@user 
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else 
+      erb :error 
+    end
   end
 
   get '/account' do
