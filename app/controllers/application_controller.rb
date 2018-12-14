b0b78ad6799518e140c3f97a59f8e1bf0e15ff6b
Greg Morgan <gregm9999@gmail.com>
2018-12-14 00:13:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user=User.find_by(username: params[:username], password[:password])
+    erb: account
   end
 
   get '/account' do
@@ -24,4 +25,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
