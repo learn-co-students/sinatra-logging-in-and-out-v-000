091d7382a3512398557eb0d17ebe5c1d760c5a3f
Samantha brake <sammy.brake@gmail.com>
2018-12-17 03:14:49 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,11 @@
 class Helpers
   
-  def current_user
-    @user 
+  def self.current_user(session_hash)
+    User.find_by(session_hash[:id])
+  end 
+  
+  def self.is_logged_in?(session_hash)
+    session_hash[:user_id] != nil 
   end 
   
   
