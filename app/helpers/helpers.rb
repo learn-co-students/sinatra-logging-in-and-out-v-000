395e354e160380be5d870cf2210c816729a12ac0
Erin Green <erin.y.green@gmail.com>
2019-01-04 21:21:51 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,15 @@
+require 'pry'
+
 class Helpers
   
-  def current_user(session)
+  def self.current_user(session)
     user_id = session[:user_id]
     current_user = User.find_by id: user_id
     current_user
   end
   
-  def is_logged_in?(session)
-    if session.contains?(:user_id)
+  def self.is_logged_in?(session)
+    if session.include?(:user_id)
       true 
     else 
       false 
