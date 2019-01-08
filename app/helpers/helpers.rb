f6d43db0e7beaa30d5a361e32f72f8d85c4cd709
Benjamin Jones <bljones36@gmail.com>
2019-01-08 06:40:08 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
-class Helpers
+class Helpers < ActiveRecord::Base
+  def self.current_user(session)
+    User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] != nil
+      true
+    else
+      false
+    end
+  end
 end
