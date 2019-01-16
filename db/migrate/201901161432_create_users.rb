4654595c9dfe68312b8e1ee4c9d96d24901f6eea
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-16 16:17:13 -0500

    Done.

--- db/migrate/201901161432_create_users.rb
+++ db/migrate/201901161432_create_users.rb
@@ -1,9 +1,9 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :username
       t.string :password
+      t.integer :balance
     end
   end
 end
