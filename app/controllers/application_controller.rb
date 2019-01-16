123c872a4c832ffd734cb5cc5f4f51371eedebaa
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2019-01-16 20:52:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,20 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    if !Helpers.is_logged_in?(session)
+      erb :error
+    else
+      erb :account
+    end
   end
 
   get '/logout' do
@@ -24,4 +33,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
