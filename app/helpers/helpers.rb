65540b8a191ada12b25fc723145b85dd572ea875
Jason Mei <jason.mei403@gmail.com>
2019-01-31 07:29:56 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session)
+    current_user = User.find(session[:user_id])
+    current_user
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
