50d4788888b5faa583c26ca7b31bf1d24b55972b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-11 20:40:16 +0000

    Done.

--- db/migrate/20190211191557_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-      create_table :users do |t|
-         t.string :name
-         t.string :password
-         t.float :balance
-     end
-  end
-end
