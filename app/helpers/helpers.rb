357bec7b6e518067563b9a6245b5c94e26e2c07c
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-02-15 21:40:39 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -9,7 +9,8 @@ class Helpers
   # to find the user in the database and return that user.
 
   def self.is_logged_in?(session)
-    !!session[:user_id] #easy way to turn it make it boolean
+    !!session[:user_id]
+    # this is a bang bang boolean easy way to turn it make it boolean - Chris Alberti
   end
   #is_logged_in? should also accept the session hash as an argument.
   #This method should return true if the user_id is in the session hash and false if not.
