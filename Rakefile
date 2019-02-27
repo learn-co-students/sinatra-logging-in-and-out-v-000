7af79af5a45c6d7ee94e2d98ce2388cf00cf6be3
Thomas Pope <tpope0928@gmail.com>
2019-02-27 16:35:11 +0000

    Done.

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@ ENV["SINATRA_ENV"] ||= "development"
 
 require 'sinatra/activerecord/rake'
 require_relative './config/environment'
+require 'sinatra/activerecord/rake'
 
 # Type `rake -T` on your command line to see the available rake tasks.
 
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -12,7 +12,7 @@ describe "Helpers" do
 
   end
 
-  describe 'Helpers.is_logged_in?' do 
+  describe 'Helpers.is_logged_in?' do
     it "returns true or false" do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
