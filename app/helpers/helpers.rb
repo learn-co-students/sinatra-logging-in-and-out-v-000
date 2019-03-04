9c748e686087c8a0cfed7b3d64d03e681cda3ad5
Ashley Wong <ashwong04@gmail.com>
2019-03-04 20:22:04 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   
-  def self.current_user
-    
+  def self.current_user(session)
+    User.find(session[:user_id])
   end
   
-  def self.is_logged_in?
-    
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
   
 end
