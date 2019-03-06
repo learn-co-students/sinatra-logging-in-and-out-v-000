5dd17792d90f705facd104d24c499bbc39987915
guysbryant <guy.s.bryant@gmail.com>
2019-03-06 06:44:58 -0500

    Learn error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user 
+      session[:id] = @user.id
+      redirct to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+    def self.current_user(session)
+        @user = User.find(session[:id])
+    end
+
+    def self.is_logged_in?(session)
+        !!(session[:id])
+    end
 end
