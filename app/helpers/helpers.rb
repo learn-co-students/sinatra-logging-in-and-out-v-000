f38a4218a7b30ea95c95b7e83b74c683e42a2031
Cheryl K <cherylkelson@icloud.com>
2019-03-27 15:57:24 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,17 +2,10 @@ class Helpers
 #These 2 methods will be called in account.erb
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
-    # @user_id
   end
 
-  def self.is_logged_in?
-
-
-
-
-
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
-
-
 
 end
