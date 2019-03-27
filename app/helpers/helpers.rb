72fb3a5b68a77096b6b2ead1b7f8f29afe185692
Cheryl K <cherylkelson@icloud.com>
2019-03-27 16:42:30 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,7 @@
+require 'sinatra/base'
+
 class Helpers
-#These 2 methods will be called in account.erb
+
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
   end
@@ -7,5 +9,4 @@ class Helpers
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,2 @@
-class User
-
-attr_accessor :username, :password, :balance
-
-
-
-
-
-
-
-
-
-
-
+class User < ActiveRecord::Base
 end
