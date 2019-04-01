d04b4baee5dfd9cc134de28d17ad79abe95bf709
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-01 21:50:48 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] == nil
+      return false
+    else
+      true
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if ActiveRecord::Base.connection.migration_context.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
 ActiveRecord::Base.logger = nil
