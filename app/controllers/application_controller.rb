a847f78438875a5ccd64499a38893fda7f009aa9
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-04-22 19:11:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,37 @@
-require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
   configure do
+    set :public_folder, 'public'
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "My session secret"
   end
 
   get '/' do
-    @session = session
     erb :index
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:login])
+    @user = User.find_by(username: params[:username])
     if @user
-      binding.pry
-      @session[:id] = @user[:id]
+      session[:user_id] = @user[:id]
       redirect '/account'
     end
+    erb :error
   end
 
   get '/account' do
-    erb :account
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ require 'capybara/rspec'
 require 'capybara/dsl'
 
 if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
 ActiveRecord::Base.logger = nil
