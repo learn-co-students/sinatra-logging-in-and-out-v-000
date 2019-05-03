4a2de631187d9aed1920242c72cf8ce6a022a591
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-05-03 16:57:51 +0000

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    user = User.find_by(username: params["username"], password: params["password"])
+  if user
+    session[:user_id] = user.id
+    redirect to '/account'
+  else
+    erb :error
   end
+end
 
-  get '/account' do
-
-  end
 
   get '/logout' do
 
   end
 
-
 end
-
