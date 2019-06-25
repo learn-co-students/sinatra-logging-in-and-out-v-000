ee6860d811dc6cb49f39c44667c4216c02254c5e
Amee Covarrubias <ameecova@yahoo.com>
2019-06-25 18:24:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    #binding.pry
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user.class == User
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end 
   end
 
   get '/account' do
-
+    #binding.pry
+    if !Helpers.is_logged_in?(session)
+      erb :error 
+    else 
+      erb :account
+    end 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
 
