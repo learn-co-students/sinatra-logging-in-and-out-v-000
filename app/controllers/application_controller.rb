886eb32d913ef4ee5b17d85d16f5cde4e7535661
Karly Mareka <karly.mareka@gmail.com>
2019-07-20 17:04:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+     @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else 
+      erb :error 
+    end
   end
+ 
 
   get '/account' do
-
+    @id = session[:user_id]
+    if @id == nil 
+      erb :error 
+    else 
+      @user = User.find_by(id: @id)
+      erb :account
+    end 
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
-class Helpers
+require 'pry'
+class Helpers < ActiveRecord::Base
+  
+  def self.current_user(user)
+    @user = User.find_by(id: user[:user_id])
+  end 
+
+  def self.is_logged_in?(user)
+    if user[:user_id]
+      true 
+    else 
+      false 
+    end 
+  end
+  
 end
