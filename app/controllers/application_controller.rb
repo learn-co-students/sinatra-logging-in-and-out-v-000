6127e5a3f4d4f2d19fd96d19e0c08b642434bd16
Steven Crouse <sdcrouse91@gmail.com>
2019-07-23 02:42:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,18 +20,64 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    # Here's the code that works:
     if Helpers.is_logged_in?(session)
-      binding.pry
       @user = Helpers.current_user(session)
-      binding.pry
       erb :account
-      binding.pry
+    else
+      erb :error
+    end
+    
+    #-----------Experimental code--------------------
+    
+    # These two sets of code work EXACTLY the same way:
+    
+    # if Helpers.is_logged_in?(session)
+    #   redirect '/user-logged-in'
+    # else
+    #   redirect '/user-not-logged-in'
+    # end
+    
+    # if Helpers.is_logged_in?(session)
+    #   redirect '/user-logged-in'
+    # end
+    # redirect '/user-not-logged-in'
+    
+    # THESE two sets of code do NOT work the same way, however, with OR without the call to #return :
+    
+    # if Helpers.is_logged_in?(session)
+    #   @user = Helpers.current_user(session)
+    #   erb :account
+    #   return
+    # end
+    # erb :error
+    
+    # if Helpers.is_logged_in?(session)
+    #   @user = Helpers.current_user(session)
+    #   erb :account
+    #   return
+    # else
+    #   erb :error
+    # end
+    # erb :error
+    
+    # Also, although #return exits out of the route (I think) and doesn't execute "erb :error", it stays on the account.erb page without rendering anything. Without the #return, the route goes onto the next line of code after the "if" statement: "erb :error".
+    
+    #-------------Previous code that doesn't completely work----------
+    
+    #if Helpers.is_logged_in?(session)
+      #binding.pry
+      #@user = Helpers.current_user(session)
+      #binding.pry
+      #erb :account
+      #binding.pry
+      #return
     #else
       #erb :error
-    end
-    binding.pry
-    erb :error
-    #-------------Previous code that doesn't completely work----------
+    #end
+    #binding.pry
+    #erb :error
+    
     #if Helpers.is_logged_in?(session)
     #binding.pry
       #@session = session
@@ -45,6 +91,14 @@ class ApplicationController < Sinatra::Base
   get '/logout' do
     session.clear
     redirect "/"
+  end
+  
+  get '/user-logged-in' do
+    "The user is logged in!"
+  end
+  
+  get '/user-not-logged-in' do
+    "The user is NOT logged in."
   end
 
 
