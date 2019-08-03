a2c8143ff36d650f3f4fb512001b5f6c4ef90847
github username <github email address>
2019-08-03 10:52:05 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
-  end
+    @user = User.find_by(username: params["username"])
+    if @user != nil 
+      session[:user_id] = @user.id
+        redirect to '/account'
+      else
+        erb :'error'
+      end
+    end
+ 
 
   get '/account' do
-
+    # user Helpers here
+    @user = Helpers.current_user(session)
+    if Helpers.is_logged_in?(session) && @user.id == session[:user_id]
+      #  binding.pry
+      erb :'account'
+    else 
+      erb :'error'
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
 
 end
 
