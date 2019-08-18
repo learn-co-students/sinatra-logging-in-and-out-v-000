033290663f9534d07a00699885a7c061c445a91f
Marcus Edwards 111 <Soulo@Marcuss-MBP.attlocal.net>
2019-08-18 15:59:17 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = Users.find_by(username: params[:username], password: params[:password], balance: params[:balance])
+    binding.pry
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    if !Helpers.is_logged_in?(sessions)
+      erb :error
+    end
+    Helpers.current_user(session)
   end
 
   get '/logout' do
-
+    session.clear
+    redirct to '/'
   end
 
 
 end
-
