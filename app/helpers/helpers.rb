e19ccce460b09c26f389cf5dfec14bff8ba2f3df
Ian Gutwinski <iangutwinski@gmail.com>
2020-03-24 19:01:25 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'sinatra/base'
+
 class Helpers
+
+    def self.current_user(session) #class method, meaning operates on the class itself, not an instance. Thereofre you don't do Helpers.new.current_user, but rather Helpers.current_user
+        @user = User.find_by(:id => session[:user_id])
+    end 
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+    end 
+
 end
