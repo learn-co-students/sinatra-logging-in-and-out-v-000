4faac64ae6a6e7ac4a7a8ffe5cf69656cd915d32
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-24 22:15:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
-    enable :sessions unless test?
+    enable :sessions #unless test?
     set :session_secret, "secret"
   end
 
@@ -11,24 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    # binding.pry
-    @user = User.find_by(:username => params[:username]) #not sure how to read this, where is the data coming from?
-    session[:user_id] = @user.id
-      # if
-      # # @user.username == session[:username]
-      #   session[:user_id] = @user.id
-      # else
-      # #incorrect password
-      # redirect to '/login'
-      # end
-redirect to '/login'
+
+    @user = User.find_by(:username => params[:username])
+  # binding.pry
+    # session[:user_id] = @user.id  #once the username is found, set the session id for the userid for that username.
+      if @user != nil && @user.password == params[:password] #if the user has a value
+      # @user.username == session[:username]
+      session[:user_id] = @user.id
+        redirect to "/account"
+      end
+      erb :error
   end
 
   get '/account' do
-
+      @user = Helpers.current_user(session)
+      if @user
+        # session[:user_id] = @user.id
+        erb :account
+      else
+        erb :error
+      end
   end
 
   get '/logout' do
+    session.clear
     redirect to '/'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, :password, :balance
 
+  # def current_user
+  #   @current_user
+  # end
+
 end
