8402045c16b56ea5fd622d6895156b3386a53309
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-06-11 22:35:42 -0700

    Productive app flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    puts params
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+    session[:user_id] = @user.id
+    redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    @user = User.find(session[:user_id])
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
