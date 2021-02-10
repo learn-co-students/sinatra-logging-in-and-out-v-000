ada803afc5c2a521e50e4bec6fcb264a581be43e
Lamia Haider <lamia.haider@gmail.com>
2021-02-10 17:34:12 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
 
+    erb :account
+
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
 
   end
 
