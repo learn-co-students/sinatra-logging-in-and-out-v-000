be9ce7d79518bf53ccaf2cc16f2d92034217fe70
ritchey0713 <ritchey0713@gmail.com>
2021-03-05 09:59:49 -0500

    fix issue with AR migration pending, and dasebases not properly creating tables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if ActiveRecord::Migrator.needs_migration?
+if ActiveRecord::Base.connection.migration_context.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
