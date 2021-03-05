786b12a10ca4f50931c52aba48fc9bbb3c9ea3c6
ritchey0713 <ritchey0713@gmail.com>
2021-03-05 10:16:09 -0500

    accidental push

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,10 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if ActiveRecord::Base.connection.migration_context.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+if ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
+
 
 ActiveRecord::Base.logger = nil
 
