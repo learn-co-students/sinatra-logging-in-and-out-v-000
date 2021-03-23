afc92294b6d1ace99a6e179a7bf1c51378029adf
github username <github email address>
2021-03-23 17:13:34 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+      end
+    
+      def self.is_logged_in?(session)
+        !!session[:user_id]
+      end
 end
+
+#the two methods will only be called in views (account.erb) 
+#helper methods make it cleaner to add logic to our views
