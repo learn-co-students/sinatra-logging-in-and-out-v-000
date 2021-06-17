608be609ffbf964232e86e10cb8c309a6e322da5
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-16 21:56:58 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,9 @@
 class Helpers
-    def self.current_user
+    def self.current_user(session)
+      @user = User.find(session[:user_id])
+    end
 
+    def self.is_logged_in?(session)
+      !!session[:user_id]
     end
 end
