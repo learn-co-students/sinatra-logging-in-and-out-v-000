8a8d87115457d3243ac583416cb50e3f4079e505
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-17 00:03:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"], balance: params["balance"])
+    @user = User.find_by(username: params[:username])
     if @user
     session[:user_id] = @user.id
     redirect '/account'
